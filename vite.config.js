071a00d3581f2832e13c6a3cf677541e275c4f50
Giovanni00000 <gicojulun046@unicah.edu>
2024-10-29 17:24:05 -0600

    otro commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import purgecss from 'vite-plugin-purgecss';
 
 export default defineConfig({
-  base: ' https://giovanni00000.github.io/portafolio/', 
+  base: "/portafolio/", 
   plugins: [purgecss()],
   css: {
     preprocessorOptions: {
